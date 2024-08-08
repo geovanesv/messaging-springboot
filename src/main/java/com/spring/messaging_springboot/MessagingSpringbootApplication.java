d0036df07bcaf04af56d9f3992eb7107fccf9921
geovanesv <geovanesv89@gmail.com>
2024-08-07 21:45:32 -0300

    iniciando projeto message kafka

---
+++ src/main/java/com/spring/messaging_springboot/MessagingSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.spring.messaging_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingSpringbootApplication.class, args);
+	}
+
+}
